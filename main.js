33b2861718f5cf76d8e6f052d30550cd96fa7219
roki-hisui-00 <roki.hisui@gmail.com>
2020-04-06 20:34:28 +0900

    require http-status-codes

--- main.js
+++ main.js
@@ -4,17 +4,20 @@
 
 // modules
 const http = require('http')
+const httpStatus = require('http-status-codes')
+
 // constant
 const DEFAULT_PORT = process.env.PORT || 3000
+const VERSION = '1.0.1'
 
 
 const app = http.createServer((req, res) => {
 
-    res.writeHead(200, {
+    res.writeHead(httpStatus.OK, {
         'Content-Type' : 'text/html'
     })
 
-    res.write('ARK')
+    res.write(`ARK ${VERSION}`)
     res.end()
 
     console.log('receive request')
@@ -22,4 +25,4 @@ const app = http.createServer((req, res) => {
 
 app.listen(DEFAULT_PORT)
 
-console.log(`--- sample start server listen: ${DEFAULT_PORT}`)
+console.log(`--- sample start server listen: ${DEFAULT_PORT} (${VERSION})`)
