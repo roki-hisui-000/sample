b3f538b9a3dbcb58595b6b846a6cdbf31da35790
roki-hisui-00 <roki.hisui@gmail.com>
2020-04-06 20:50:48 +0900

    add /info path

--- main.js
+++ main.js
@@ -11,12 +11,17 @@ const app = express()
 
 // constant
 const DEFAULT_PORT = process.env.PORT || 3000
-const VERSION = '1.1.0'
+const VERSION = '1.1.1'
 const MSG = `ARK ${VERSION}`
 
 app.get('/', (req, res) => {
     res.send(MSG)
 })
+
+app.get('/info', (req, res) => {
+    res.send('access "/info" path')
+})
+
 
 app.listen(DEFAULT_PORT, () => {
     console.log(`--- sample start server listen: ${DEFAULT_PORT} (${VERSION})`)
